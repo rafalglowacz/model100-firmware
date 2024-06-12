83330fcf55a5d23110869cf6fe34f3583d519a1f
Rafał Głowacz <rafalglowacz@gmail.com>
2024-06-12 19:24:56 +0200

    remove comments from the includes section

--- Model100.ino
+++ Model100.ino
@@ -1,103 +1,35 @@
 // -*- mode: c++ -*-
-// Copyright 2016-2022 Keyboardio, inc. <jesse@keyboard.io>
-// See "LICENSE" for license details
-
-/**
- * These #include directives pull in the Kaleidoscope firmware core,
- * as well as the Kaleidoscope plugins we use in the Model 100's firmware
- */
-
-// The Kaleidoscope core
 #include "Kaleidoscope.h"
-
-// Support for storing the keymap in EEPROM
 #include "Kaleidoscope-EEPROM-Settings.h"
 #include "Kaleidoscope-EEPROM-Keymap.h"
-
-// Support for communicating with the host via a simple Serial protocol
 #include "Kaleidoscope-FocusSerial.h"
-
-// Support for querying the firmware version via Focus
 #include "Kaleidoscope-FirmwareVersion.h"
-
-// Support for keys that move the mouse
 #include "Kaleidoscope-MouseKeys.h"
-
-// Support for macros
 #include "Kaleidoscope-Macros.h"
-
-// Support for controlling the keyboard's LEDs
 #include "Kaleidoscope-LEDControl.h"
-
-// Support for "Numpad" mode, which is mostly just the Numpad specific LED mode
 #include "Kaleidoscope-NumPad.h"
-
-// Support for the "Boot greeting" effect, which pulses the 'LED' button for 10s
-// when the keyboard is connected to a computer (or that computer is powered on)
 #include "Kaleidoscope-LEDEffect-BootGreeting.h"
-
-// Support for LED modes that set all LEDs to a single color
 #include "Kaleidoscope-LEDEffect-SolidColor.h"
-
-// Support for an LED mode that makes all the LEDs 'breathe'
 #include "Kaleidoscope-LEDEffect-Breathe.h"
-
-// Support for an LED mode that makes a red pixel chase a blue pixel across the keyboard
 #include "Kaleidoscope-LEDEffect-Chase.h"
-
-// Support for LED modes that pulse the keyboard's LED in a rainbow pattern
 #include "Kaleidoscope-LEDEffect-Rainbow.h"
-
-// Support for an LED mode that lights up the keys as you press them
 #include "Kaleidoscope-LED-Stalker.h"
-
-// Support for an LED mode that prints the keys you press in letters 4px high
 #include "Kaleidoscope-LED-AlphaSquare.h"
-
-// Support for shared palettes for other plugins, like Colormap below
 #include "Kaleidoscope-LED-Palette-Theme.h"
-
-// Support for an LED mode that lets one configure per-layer color maps
 #include "Kaleidoscope-Colormap.h"
-
-// Support for turning the LEDs off after a certain amount of time
 #include "Kaleidoscope-IdleLEDs.h"
-
-// Support for setting and saving the default LED mode
 #include "Kaleidoscope-DefaultLEDModeConfig.h"
-
-// Support for changing the brightness of the LEDs
 #include "Kaleidoscope-LEDBrightnessConfig.h"
-
-// Support for Keyboardio's internal keyboard testing mode
 #include "Kaleidoscope-HardwareTestMode.h"
-
-// Support for host power management (suspend & wakeup)
 #include "Kaleidoscope-HostPowerManagement.h"
-
-// Support for magic combos (key chords that trigger an action)
 #include "Kaleidoscope-MagicCombo.h"
-
-// Support for USB quirks, like changing the key state report protocol
 #include "Kaleidoscope-USB-Quirks.h"
-
-// Support for secondary actions on keys
 #include "Kaleidoscope-Qukeys.h"
-
-// Support for one-shot modifiers and layer keys
 #include "Kaleidoscope-OneShot.h"
 #include "Kaleidoscope-Escape-OneShot.h"
-
-// Support for dynamic, Chrysalis-editable macros
 #include "Kaleidoscope-DynamicMacros.h"
-
-// Support for SpaceCadet keys
 #include "Kaleidoscope-SpaceCadet.h"
-
-// Support for editable layer names
 #include "Kaleidoscope-LayerNames.h"
-
-// Support for the GeminiPR Stenography protocol
 #include "Kaleidoscope-Steno.h"
 
 /** This 'enum' is a list of all the macros used by the Model 100's firmware
