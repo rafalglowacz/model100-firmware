b271c033bc67ef4397d09f6b5ee31db6be603517
Rafał Głowacz <rafalglowacz@gmail.com>
2024-06-17 19:08:06 +0200

    remove orignal primary layer, leave only custom

--- Model100.ino
+++ Model100.ino
@@ -63,39 +63,12 @@ enum {
   SYMBOL,
 };
 
-// #define PRIMARY_KEYMAP_QWERTY
-#define PRIMARY_KEYMAP_CUSTOM
-
 /* This comment temporarily turns off astyle's indent enforcement
  * so we can make the keymaps actually resemble the physical key layout better
  */
 // clang-format off
 
 KEYMAPS(
-
-#if defined (PRIMARY_KEYMAP_QWERTY)
-
-  // [PRIMARY] = KEYMAP_STACKED
-  // (___,          Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext,
-  //  Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
-  //  Key_PageUp,   Key_A, Key_S, Key_D, Key_F, Key_G,
-  //  Key_PageDown, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,
-  //
-  //
-  //  Key_LeftControl, Key_Backspace, Key_LeftGui, Key_LeftShift,
-  //  ShiftToLayer(FUNCTION),
-  //
-  //      M(MACRO_ANY),  Key_6, Key_7, Key_8,     Key_9,         Key_0,         LockLayer(NUMPAD),
-  //      Key_Enter,     Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
-  //                     Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
-  //      Key_RightAlt,  Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
-  //
-  //      Key_RightShift, Key_LeftAlt, Key_Spacebar, Key_RightControl,
-  //      ShiftToLayer(FUNCTION)),
-
-#elif defined (PRIMARY_KEYMAP_CUSTOM)
-
-  // Edit this keymap to make a custom layout
   [PRIMARY] = KEYMAP_STACKED
   (___,          ___,   ___,   ___,   ___,   ___,   Key_LEDEffectNext,
    Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
@@ -113,10 +86,6 @@ KEYMAPS(
 
        ___, Key_Backspace, ShiftToLayer(MOUSE), Key_Spacebar,
        ShiftToLayer(NUMPAD_ALT)),
-
-#else
-#error "No default keymap defined. You should make sure that you have a line like '#define PRIMARY_KEYMAP_QWERTY' in your sketch"
-#endif
 
   [NUMPAD] =  KEYMAP_STACKED
   (___, ___, ___, ___, ___, ___, ___,
